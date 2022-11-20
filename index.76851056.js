e83a8c62c428b23d98b523487176f989e65bcec4
Yaroslav-Marinich <113300647+Yaroslav-Marinich@users.noreply.github.com>
2022-11-20 14:49:25 +0000

    Deploying to gh-pages from @ stark1269/fson-68-dmt@181affc9f39d94eab06dcb78e546100a22f2fe35 🚀

---
+++ index.76851056.js
@@ -0,0 +1,2 @@
+$(".one-time").slick({dots:!0,arrows:!1,infinite:!0,speed:300,slidesToShow:1,adaptiveHeight:!0,autoplay:!0,autoplaySpeed:5e3});
+//# sourceMappingURL=index.76851056.js.map
---
+++ index.9a4b8c0d.js
@@ -0,0 +1,2 @@
+$(".one-time").slick({dots:!0,arrows:!1,infinite:!0,speed:300,slidesToShow:1,adaptiveHeight:!0,autoplay:!0,autoplaySpeed:5e3});
+//# sourceMappingURL=index.9a4b8c0d.js.map
